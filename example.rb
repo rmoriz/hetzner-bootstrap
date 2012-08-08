5cb71202590690d055589cf3ad53a8c4e184ebe6
Roland Moriz <roland@moriz.de>
2012-08-08 19:49:55 +0300

    updated example

--- example.rb
+++ example.rb
@@ -18,27 +18,89 @@ DRIVE1 /dev/sda
 DRIVE2 /dev/sdb
 FORMATDRIVE2 0
 
+## ===============
+##  SOFTWARE RAID:
+## ===============
+
+## activate software RAID?  < 0 | 1 >
 SWRAID 1
+
+## Choose the level for the software RAID < 0 | 1 >
 SWRAIDLEVEL 1
 
+## which bootloader should be used?  < lilo | grub >
 BOOTLOADER grub
 
 HOSTNAME <%= hostname %>
+
+## PART  <mountpoint/lvm>  <filesystem/VG>  <size in MB>
+##
+## * <mountpoint/lvm> mountpoint for this filesystem  *OR*  keyword 'lvm'
+##                    to use this PART as volume group (VG) for LVM
+## * <filesystem/VG>  can be ext2, ext3, reiserfs, xfs, swap  *OR*  name
+##                    of the LVM volume group (VG), if this PART is a VG
+## * <size>           you can use the keyword 'all' to assign all the
+##                    remaining space of the drive to the *last* partition.
+##                    you can use M/G/T for unit specification in MIB/GIB/TIB
+## 
+## notes:
+##   - extended partitions are created automatically
+##   - '/boot' cannot be on a xfs filesystem!
+##   - '/boot' cannot be on LVM!
+##   - when using software RAID 0, you need a '/boot' partition
 
 PART /boot ext2    1G
 PART lvm   host   75G
 PART lvm   guest  all
 
+#LV <VG> <name> <mount> <filesystem> <size>
 LV host root /    ext3  50G
 LV host swap swap swap   5G
 
+
+## ========================
+##  OPERATING SYSTEM IMAGE:
+## ========================
+
+## full path to the operating system image
+##   supported image sources:  local dir,  ftp,  http,  nfs
+##   supported image types:  tar,  tar.gz,  tar.bz,  tar.bz2,  tgz,  tbz
+## examples:
+#
+# local: /path/to/image/filename.tar.gz
+# ftp:   ftp://<user>:<password>@hostname/path/to/image/filename.tar.bz2
+# http:  http://<user>:<password>@hostname/path/to/image/filename.tbz
+# https: https://<user>:<password>@hostname/path/to/image/filename.tbz
+# nfs:   hostname:/path/to/image/filename.tgz
+
+# Default images provided by hetzner as of August 2012:
+# CentOS-58-32-minimal.tar.gz
+# CentOS-58-64-minimal.tar.gz
+# CentOS-62-32-minimal.tar.gz
+# CentOS-62-64-cpanel.tar.gz
+# CentOS-62-64-minimal.tar.gz
+# CentOS-63-32-minimal.tar.gz
+# CentOS-63-64-cpanel.tar.gz
+# CentOS-63-64-minimal.tar.gz
+# Debian-60-squeeze-32-minimal.tar.gz
+# Debian-60-squeeze-64-LAMP.tar.gz
+# Debian-60-squeeze-64-minimal.tar.gz
+# openSUSE-121-32-minimal.tar.gz
+# openSUSE-121-64-minimal.tar.gz
+# Ubuntu-1004-lucid-32-minimal.tar.gz
+# Ubuntu-1004-lucid-64-minimal.tar.gz
+# Ubuntu-1204-precise-32-minimal.tar.gz
+# Ubuntu-1204-precise-64-minimal.tar.gz
+
+
 IMAGE /root/images/Ubuntu-1204-precise-64-minimal.tar.gz
+
 EOT
 
-# the post_install hook is a great place to setup software/system provisioning
+# the post_install hook is a great place to setup further software/system provisioning
 #
 post_install = <<EOT
-knife bootstrap <%= ip %> -N <%= hostname %> "role[base],role[kvm_host]"
+  # knife bootstrap <%= ip %> -N <%= hostname %> "role[base],role[kvm_host]"
 EOT
 
 # duplicate entry for each system
